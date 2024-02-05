42d9ec8dd18309f9dc0e75811f593a225c5649d3
Manoj <01fe22bca083@kletech.ac.in>
2024-02-05 12:21:18 +0530

    code for square and cube formalties

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -11,6 +11,14 @@ class BasicCalculator{
     float division (float num_1,float num_2){
         return(num_1 / num_2);
     }
+    
+    public float square(float x) {
+        return (x * x);
+    }
+
+    public float cube(float x) {
+        return (x * x * x);
+    }
     public static void main(String[] args) {
         float num_1 =10;
         float num_2 = 5;
@@ -23,6 +31,8 @@ class BasicCalculator{
         System.out.println("Result of Addition is :" + mul_result);
         float div_result = calc.division(num_1, num_2);
         System.out.println("Result of Addition is :" + div_result);
+        System.out.print("\nSquare of " + num_1+ " is: " + calc.square(num_1));
+        System.out.print("\nCube of " + num_1 + " is: " + calc.cube(num_1));
 
     }
 }
