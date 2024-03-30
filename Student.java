31ba8af7a6c71d6e42a3936676aade39645062b7
Manoj <01fe22bca083@kletech.ac.in>
2024-03-30 23:40:19 +0530

    Extended code to display student details

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.time.*;
+
+public class Student extends student_course_info
+{  
+    public static void main(String[] args)
+    { 
+        String stud_name = "Manoj";
+
+        LocalDate pdt = LocalDate.of(2004,07,16);
+        LocalDate tdt = LocalDate.now();
+        Period diff = Period.between(pdt, tdt);
+        System.out.println("Name:" +stud_name);
+        System.out.println("Date of Birth : "+pdt);
+        System.out.println("Age : "+diff.getYears());
+        System.out.println("Course and marks of semester 4:");
+        student_course_info sc = new student_course_info();
+        sc.stud_info();
+            
+    }
+}
