3bf74b66a78b0d5dd5f5449f8798b54e600a9815
manoj <01fe22bca83@kletech.ac.in>
2024-03-31 00:13:59 +0530

    extended code

--- Student.java
+++ Student.java
@@ -8,8 +8,7 @@ public class Student extends student_course_info
 
         LocalDate pdt = LocalDate.of(2004,07,16);
         LocalDate tdt = LocalDate.now();
-        Period diff = Period.between(pdt, tdt);
-        System.out.println("Name:" +stud_name);
+        Period diff = Period.between(pdt, tdt);        System.out.println("Name:" +stud_name);
         System.out.println("Date of Birth : "+pdt);
         System.out.println("Age : "+diff.getYears());
         System.out.println("Course and marks of semester 4:");
